a7951b0be8384021950299f1dec9342ce52a66de
Lisa Andersen <lisa@lisa-andersen.dk>
2026-04-21 20:13:43 +0200

    Initial scripts
    These scripts are originally taken from https://github.com/mcobit/retrosmc
    and modified by https://github.com/ornostar as described in https://github.com/mcobit/retrosmc/issues/67.
    I cannot and should not and should not take any credit for this.

---
+++ scripts/retropie.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+cd /home/osmc
+
+# This scripts starts the emulationstation watchdog deamon and
+# emulationstation itself while stopping KODI afterwards.
+# Script by mcobit
+# moved content to retropie_starter.sh. Just kept the systemctl stop mediacenter + added the call of retropie_starter.
+
+# stop kodi to free input devices for emulationstation
+echo "retropie.sh: call retropie_starter.sh" > retropiestarter.log
+sudo su osmc -c "sh /home/osmc/RetroPie/scripts/retropie_starter.sh &" &
+sleep 1
+echo "retropie.sh: call systemctl stop mediacenter" >> retropiestarter.log
+sudo su -c "systemctl stop mediacenter" 
+
+echo "retropie.sh: done exit" >> retropiestarter.log
+exit
+
